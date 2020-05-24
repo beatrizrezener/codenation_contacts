aeeba59b24e020b265305814112ffb008351eb5b
Beatriz Rezener <beatrizrezener@gmail.com>
2020-05-24 18:37:12 -0300

    Updates

---
+++ precache-manifest.561af41fe4347d3ec88c439845c2bed6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ce25a8ce844d456aaa53c4acea155b",
+    "url": "/codenation_contacts/index.html"
+  },
+  {
+    "revision": "cb5630911539e675c54c",
+    "url": "/codenation_contacts/static/css/main.83b78fd8.chunk.css"
+  },
+  {
+    "revision": "dfcc89f6c88ddc13d6e7",
+    "url": "/codenation_contacts/static/js/2.0f28186c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codenation_contacts/static/js/2.0f28186c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5630911539e675c54c",
+    "url": "/codenation_contacts/static/js/main.1abf4708.chunk.js"
+  },
+  {
+    "revision": "642bc9e4636f7bf37b34",
+    "url": "/codenation_contacts/static/js/runtime-main.d1e99871.js"
+  },
+  {
+    "revision": "82a7c06238e831cd8f70bf1182156c2e",
+    "url": "/codenation_contacts/static/media/logo.82a7c062.svg"
+  }
+]);
